1dcab9d91af323b876d818c6a9592acb744eca2f
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-16 14:26:21 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -26,4 +26,4 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-APP = "@MISS_AARU_MUSIC_BOT"  # connect music api key "Dont change it"
+APP = "@vip_music_vc_bot"  # connect music api key "Dont change it"

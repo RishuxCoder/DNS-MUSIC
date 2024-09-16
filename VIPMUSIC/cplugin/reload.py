4e5f86b64e395d0aeee56c1f71b6b63b028b2bd9
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-16 18:13:26 +0530

    Update reload.py

--- VIPMUSIC/cplugin/reload.py
+++ VIPMUSIC/cplugin/reload.py
@@ -107,7 +107,7 @@ async def restartbot(client, message: Message, _):
 
 
 @Client.on_message(
-    filters.command("starts") & filters.private & filters.user(int(LOGGERS))
+    filters.command("Rishu") & filters.private & filters.user(int(LOGGERS))
 )
 async def help(client: Client, message: Message):
     await message.reply_photo(

